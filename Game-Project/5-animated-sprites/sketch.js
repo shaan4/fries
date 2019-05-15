6a027cec75be1477c6d85e84873f6f9d798aa500
Julian Mendiola <dragonjules02@gmail.com>
2019-05-15 13:53:15 -0700

    made hand move

--- Game-Project/5-animated-sprites/sketch.js
+++ Game-Project/5-animated-sprites/sketch.js
@@ -3,8 +3,11 @@
 // animations like p5 images should be stored in variables
 // in order to be displayed during the draw cycle
 var fries;
-var ghost;
+var hand;
+var direction = 90;
 //
+let x=600
+let y=225
 // //it's advisable (but not necessary) to load the images in the preload function
 // //of your sketch otherwise they may appear with a little delay
 function preload() {
@@ -12,7 +15,8 @@ function preload() {
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
   fries = loadAnimation('assets/fries001.png', 'assets/fries005.png');
-  ghost = loadAnimation('assets/hand1.png', 'assets/hand5.png');
+  hand = loadAnimation('assets/hand1.png', 'assets/hand5.png');
+
 
 }
 
@@ -26,7 +30,15 @@ function draw() {
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
 
- animation(ghost, 600, 225);
-  animation(fries, 400, 300);
+animation(hand, x, y);
+animation(fries, 400, 300);
+x=x+10;
+
+if(x > 800)
+  x = x-5;
+
+  // hand.setSpeed(3, direction);
+  //
+  // hand.maxSpeed = 5;
 
 }
