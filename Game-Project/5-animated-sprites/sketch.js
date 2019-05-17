b43b79a08045cbe68ab01a8233dbb43dd3918d3a
Lizzy Brooks <lizzy.brooks@gmail.com>
2019-05-17 09:46:58 -0700

    put the hand animation into a class
    I combined the avatar class from conditional statements with the hand animation. If this isn't what you want, you can easily undo it.

--- Game-Project/5-animated-sprites/sketch.js
+++ Game-Project/5-animated-sprites/sketch.js
@@ -10,6 +10,11 @@ let x=600
 let y=225
 // //it's advisable (but not necessary) to load the images in the preload function
 // //of your sketch otherwise they may appear with a little delay
+
+//create a variable to hold your avatar
+let me;
+
+
 function preload() {
 //
 //   //create an animation from a sequence of numbered images
@@ -22,23 +27,91 @@ function preload() {
 
 function setup() {
   createCanvas(800, 600);
+  //make one avatar called me
+  me = new Avatar(600, 200, 3);
 }
 
 function draw() {
   background(200, 255, 255);
+  me.drawMe();
+  me.moveMe();
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
 
-animation(hand, x, y);
-animation(fries, 400, 300);
-x=x+10;
 
-if(x > 800)
-  x = x-5;
+animation(fries, 400, 300);
+// x=x+10;
+//
+// if(x > 800)
+//   x = x-5;
 
   // hand.setSpeed(3, direction);
   //
   // hand.maxSpeed = 5;
+
+}
+
+//avatar class
+class Avatar {
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+	drawMe(){  // draw the running person
+    animation(hand, this.x, this.y);
+
+	}
+
+	moveMe(){
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move up by speed
+       this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+        this.x += this.speed;
+    }
+	}
+
+  die(){
+
+  }
+
+}
+
+
+//ball class from which to create new balls with similar properties.
+class Ball {
+
+	//every ball needs an x value, a y value, and a speed
+	constructor(x,y, speed){
+		this.x = x;
+    this.y = y;
+    this.speed = speed;
+	}
+
+	// draw a ball on the screen at x,y
+	drawBall(){
+    	stroke(0);
+      strokeWeight(1);
+    	fill("red");
+		  ellipse(this.x,this.y,10,10);
+	}
+
+	//update the location of the ball, so it moves across the screen
+	moveBall(){
+		this.x = this.x+ this.speed;
+		this.y = this.y+.5;
+	}
+
+	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
+  	bounceBall(){
+    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+      			this.speed = -this.speed;
+    		}
+  	}
 
 }
